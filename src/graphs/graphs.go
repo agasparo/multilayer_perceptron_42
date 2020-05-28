2244b326d2d99d1d7763157b3d0b86a5b9c36da3
arthur <arthurysiite@gmail.Com>
2020-05-28 12:20:44 +0200

    change learning rate

--- src/graphs/graphs.go
+++ src/graphs/graphs.go
@@ -8,15 +8,39 @@ import (
 
 func Draw(data []network.Save) {
 
+	for i := 0; i < len(data); i++ {
+
+		LearningRate(Prepare(len(data[i].Lr)), data[i].Lr, data[i].Lr_t)
+	}
+}
+
+func LearningRate(x, y []float64, name string) {
+
 	graph := chart.Chart{
+		XAxis: chart.XAxis{
+			Name: "epochs",
+		},
+		YAxis: chart.YAxis{
+			Name: "Learning rate",
+		},
 		Series: []chart.Series{
 			chart.ContinuousSeries{
-				XValues: []float64{1.0, 2.0, 3.0, 4.0, 5.0},
-				YValues: []float64{1.0, 2.0, 3.0, 4.0, 5.0},
+				XValues: x,
+				YValues: y,
 			},
 		},
 	}
-	f, _ := os.Create("data/view/output.png")
+	f, _ := os.Create("data/view/learningRate_" + name + ".png")
 	defer f.Close()
 	graph.Render(chart.PNG, f)
+}
+
+func Prepare(size int) ([]float64) {
+
+	var table []float64
+
+	for i := 0; i < size; i++ {
+		table = append(table, float64(i))
+	}
+	return (table)
 }
